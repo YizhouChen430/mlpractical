ed0e2b49b985ebeb357046408c1e59bc5fb246c3
Mac Chen <chenyzzz430@gmail.com>
2022-12-01 23:40:56 +0000

    Update experiment_builder.py

--- pytorch_mlp_framework/experiment_builder.py
+++ pytorch_mlp_framework/experiment_builder.py
@@ -149,9 +149,7 @@ class ExperimentBuilder(nn.Module):
        
         """
         all_grads = []
-        conv_grads = []
         layers = []
-        conv_layers = []
 
         """
         Complete the code in the block below to collect absolute mean of the gradients for each layer in all_grads with the layer names in layers.
@@ -166,16 +164,40 @@ class ExperimentBuilder(nn.Module):
                     name = layer_splits[1] + '_' + layer_splits[3]
                 layers.append(name)
                 all_grads.append(torch.mean(torch.abs(param.grad)))
+
+        ########################################
+
+        plt_full = self.plot_func_def(all_grads, layers)
+        return plt_full
+
+    def plot_grad_flow(self, named_parameters):
+        """
+        The function is being called in Line 298 of this file.
+        Receives the parameters of the model being trained. Returns plot of gradient flow for the given model parameters.
+
+        """
+        conv_grads = []
+        conv_layers = []
+
+        """
+        Complete the code in the block below to collect absolute mean of the gradients for each layer in all_grads with the layer names in layers.
+        """
+        ########################################
+        for layer, param in named_parameters:
+            if param.requires_grad and 'weight' in layer:
+                layer_splits = layer.split('.')
+                if 'logit' in layer_splits[0]:
+                    name = layer_splits[0]
+                else:
+                    name = layer_splits[1] + '_' + layer_splits[3]
                 if 'bn' not in layer:
                     conv_layers.append(name)
                     conv_grads.append(torch.mean(torch.abs(param.grad)))
 
         ########################################
-
-        plt_full = self.plot_func_def(all_grads, layers)
         plt_conv = self.plot_func_def(conv_grads, conv_layers)
 
-        return plt_full, plt_conv
+        return plt_conv
 
     def run_train_iter(self, x, y):
 
@@ -306,15 +328,16 @@ class ExperimentBuilder(nn.Module):
             ################################################################
             ##### Plot Gradient Flow at each Epoch during Training  ######
             print("Generating Gradient Flow Plot at epoch {}".format(epoch_idx))
-            plt_full, plt_conv = self.plot_grad_flow(self.model.named_parameters())
+            plt_full = self.plot_grad_flow(self.model.named_parameters())
             if not os.path.exists(os.path.join(self.experiment_saved_models, 'gradient_flow_plots')):
                 os.mkdir(os.path.join(self.experiment_saved_models, 'gradient_flow_plots'))
                 # plt.legend(loc="best")
-            if not os.path.exists(os.path.join(self.experiment_saved_models, 'gradient_flow_plots_conv')):
-                os.mkdir(os.path.join(self.experiment_saved_models, 'gradient_flow_plots_conv'))
-                #
             plt_full.savefig(
                 os.path.join(self.experiment_saved_models, 'gradient_flow_plots', "epoch{}.pdf".format(str(epoch_idx))))
+
+            if not os.path.exists(os.path.join(self.experiment_saved_models, 'gradient_flow_plots_conv')):
+                os.mkdir(os.path.join(self.experiment_saved_models, 'gradient_flow_plots_conv'))
+            plt_conv = self.plot_grad_flow_conv(self.model.named_parameters())
             plt_conv.savefig(os.path.join(self.experiment_saved_models, 'gradient_flow_plots_conv',
                                           "epoch{}.pdf".format(str(epoch_idx))))
             ################################################################
